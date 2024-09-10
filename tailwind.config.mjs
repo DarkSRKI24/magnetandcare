f4353a120d3bfcb386bb4acab7e8569ab2907320
DarkSRKI24 <darksrki@gmail.com>
2024-09-10 20:03:04 +0200

    s

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,8 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      fontFamily: {
-        montserrat: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
-      },
       colors: {
         text: '#071111',
         background: '#f5fbfc',
