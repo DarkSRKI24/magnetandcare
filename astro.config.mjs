d341e2639c5c70eab4ac25e4c9364ea15045387e
DarkSRKI24 <darksrki@gmail.com>
2024-10-12 06:13:43 +0200

    Turnstile

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import tailwind from '@astrojs/tailwind';
 
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+
   i18n: {
     defaultLocale: "sr",
     locales: ["sr", "en", "de"],
-  }
+  },
+
+  output: "server",
+  adapter: cloudflare()
 });
